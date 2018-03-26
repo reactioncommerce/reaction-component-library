42d8adc0cd66946e07dc69587cfc7b5feadea1e8
Eric Dobbertin <eric@dairystatedesigns.com>
2018-03-26 17:32:08 -0500

    refactor: hard code theme active and hover color vals

--- src/defaultComponentTheme.js
+++ src/defaultComponentTheme.js
@@ -1,5 +1,4 @@
 /* eslint-disable camelcase */
-import { darken, lighten, saturate } from 'polished';
 
 const reactionBlue = "#1999dd";
 const coolGrey = "#5e7480";
@@ -9,12 +8,17 @@ const coolGrey500 = "#3c3c3c";
 const red = "#cd3f4c";
 const white = "#ffffff";
 
-"#4E8FA7"
+const redActive = "#bc1d2b";
+const coolGreyActive = "#3b5c70";
+const coolGreyActiveLight = "#c9d9e1";
+const reactionBlueActive = "#057dbe";
+const coolGrey400Active = "#214357";
 
-const darkActive = (val) => saturate(0.15, darken(0.1, val));
-const lightActive = (val) => saturate(0.15, lighten(0.4, val));
-const lightHover1 = (val) => saturate(0.15, lighten(0.08, val));
-const lightHover2 = (val) => saturate(0.15, lighten(0.5, val));
+const redHover = "#e4505e";
+const coolGreyHover = "#5d8fa9";
+const coolGreyHoverLight = "#eaeff3";
+const reactionBlueHover = "#25b0f9";
+const coolGrey400Hover = "#3a7899";
 
 const paddingBasePixels = 10;
 
@@ -25,27 +29,27 @@ export default {
   rui_buttonBorderColor_disabled: coolGrey200,
   rui_buttonBorderColor_important: reactionBlue,
   rui_buttonBorderColor_secondary: coolGrey,
-  rui_buttonBorderColor_danger_active: darkActive(red),
-  rui_buttonBorderColor_default_active: darkActive(coolGrey),
-  rui_buttonBorderColor_important_active: darkActive(reactionBlue),
+  rui_buttonBorderColor_danger_active: redActive,
+  rui_buttonBorderColor_default_active: coolGreyActive,
+  rui_buttonBorderColor_important_active: reactionBlueActive,
   rui_buttonBorderColor_secondary_active: coolGrey,
-  rui_buttonBorderColor_danger_hover: lightHover1(red),
-  rui_buttonBorderColor_default_hover: lightHover1(coolGrey),
-  rui_buttonBorderColor_important_hover: lightHover1(reactionBlue),
+  rui_buttonBorderColor_danger_hover: redHover,
+  rui_buttonBorderColor_default_hover: coolGreyHover,
+  rui_buttonBorderColor_important_hover: reactionBlueHover,
   rui_buttonBorderColor_secondary_hover: coolGrey,
   rui_buttonBackgroundColor_danger: red,
   rui_buttonBackgroundColor_default: coolGrey,
   rui_buttonBackgroundColor_disabled: coolGrey200,
   rui_buttonBackgroundColor_important: reactionBlue,
   rui_buttonBackgroundColor_secondary: "transparent",
-  rui_buttonBackgroundColor_danger_active: darkActive(red),
-  rui_buttonBackgroundColor_default_active: darkActive(coolGrey),
-  rui_buttonBackgroundColor_important_active: darkActive(reactionBlue),
-  rui_buttonBackgroundColor_secondary_active: lightActive(coolGrey),
-  rui_buttonBackgroundColor_danger_hover: lightHover1(red),
-  rui_buttonBackgroundColor_default_hover: lightHover1(coolGrey),
-  rui_buttonBackgroundColor_important_hover: lightHover1(reactionBlue),
-  rui_buttonBackgroundColor_secondary_hover: lightHover2(coolGrey),
+  rui_buttonBackgroundColor_danger_active: redActive,
+  rui_buttonBackgroundColor_default_active: coolGreyActive,
+  rui_buttonBackgroundColor_important_active: reactionBlueActive,
+  rui_buttonBackgroundColor_secondary_active: coolGreyActiveLight,
+  rui_buttonBackgroundColor_danger_hover: redHover,
+  rui_buttonBackgroundColor_default_hover: coolGreyHover,
+  rui_buttonBackgroundColor_important_hover: reactionBlueHover,
+  rui_buttonBackgroundColor_secondary_hover: coolGreyHoverLight,
   rui_buttonForegroundColor_danger: white,
   rui_buttonForegroundColor_default: white,
   rui_buttonForegroundColor_disabled: white,
@@ -62,8 +66,8 @@ export default {
   rui_buttonHorizontalPadding: `${paddingBasePixels*2}px`,
   rui_buttonMinimumWidth: "100px",
   rui_buttonTextOnlyColor_default: coolGrey400,
-  rui_buttonTextOnlyColor_default_active: darkActive(coolGrey400),
-  rui_buttonTextOnlyColor_default_hover: lightHover1(coolGrey400),
+  rui_buttonTextOnlyColor_default_active: coolGrey400Active,
+  rui_buttonTextOnlyColor_default_hover: coolGrey400Hover,
   rui_buttonTextOnlyColor_disabled: coolGrey200,
   rui_buttonVerticalPadding: `${paddingBasePixels}px`,
   rui_buttonVerticalPaddingShort: `${paddingBasePixels/2}px`
