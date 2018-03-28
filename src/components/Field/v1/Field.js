a228fa5894f1291ad95c9c9e9aded6ce604afc61
Nat Hamilton <info@nathamilton.com>
2018-03-28 10:44:43 -0500

    feat: added lodash.isEmpty, started field component, added the field to the styleguide config

--- src/components/Field/v1/Field.js
+++ src/components/Field/v1/Field.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import isEmpty from 'lodash.isempty';
 import { applyTheme } from "helpers";
 
 const StyledDiv = styled.div`
@@ -8,19 +9,52 @@ const StyledDiv = styled.div`
 `;
 
 class Field extends Component {
-  static propTypes = {
+  static isFormField = true;
 
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    errors: PropTypes.array,
+    isRequired: PropTypes.bool,
+    label: PropTypes.node,
+    labelClassName: PropTypes.string,
+    labelFor: PropTypes.string
   };
 
   static defaultProps = {
-
+    className: undefined,
+    errors: undefined,
+    label: undefined,
+    labelClassName: undefined,
+    labelFor: undefined,
+    isRequired: false
   };
 
-  render() {
+  getClassName() {
+    const { className, errors, isRequired } = this.props;
+    const errorClass = Array.isArray(errors) && errors.length > 0 ? 'has-error' : '';
+    const requiredClass = isRequired ? 'required' : '';
+    return `${className || ''} ${errorClass} ${requiredClass}`.trim();
+  }
+
+  renderLabel() {
+    const { label, labelClassName, labelFor } = this.props;
+
     return (
-      <StyledDiv>TEST</StyledDiv>
+      <label className={labelClassName} htmlFor={labelFor}>{label}</label>
     );
   }
+
+  render() {
+    const { children, label } = this.props;
+
+    return (
+      <div className={this.getClassName()}>
+        {!isEmpty(label) && this.renderLabel()}
+        {children}
+      </div>
+    );
+}
 }
 
 export default Field;
