acbe52314971745cb1c023732b460489b0a53f15
Eric Dobbertin <eric@dairystatedesigns.com>
2018-03-30 16:21:39 -0500

    chore: update build script

--- .reaction/scripts/prepublish.js
+++ .reaction/scripts/prepublish.js
@@ -2,27 +2,29 @@ const fs = require("fs-extra");
 const path = require("path");
 
 const BASE_PATH = path.join(process.cwd(), "src/components");
+const SRC_PATH = path.join(process.cwd(), "src");
 
 const versions = {};
 const directoryContents = fs.readdirSync(BASE_PATH);
 directoryContents.forEach((componentName) => {
-  console.log("componentName", componentName);
   if (componentName.indexOf(".") !== -1) return;
   const compDirectoryContents = fs.readdirSync(path.join(BASE_PATH, componentName));
   compDirectoryContents.forEach((versionName) => {
     if (!versionName.startsWith("v")) return;
     if (!versions[versionName]) versions[versionName] = [];
-    versions[versionName].push(`export { default as ${componentName} } from "./${componentName}/v1/${componentName}";`)
+    versions[versionName].push(`export { default as ${componentName} } from "./components/${componentName}/v1/${componentName}";`)
   });
 });
 
 Object.keys(versions).forEach((version) => {
   const lines = versions[version];
-  const filePath = path.join(BASE_PATH, `${version}.js`);
+  const filePath = path.join(SRC_PATH, `${version}.js`);
   fs.ensureFileSync(filePath);
   fs.writeFileSync(filePath, lines.join("\n") + "\n");
 });
 
-const indexFilePath = path.join(BASE_PATH, "index.js");
+const indexFilePath = path.join(SRC_PATH, "index.js");
 fs.ensureFileSync(indexFilePath);
-fs.writeFileSync(indexFilePath, Object.keys(versions).map((version) => `export * as ${version} from "./${version}";`).join("\n") + "\n");
+let content = Object.keys(versions).map((version) => `import * as ${version} from "./${version}";`).join("\n") + "\n\n";
+content += `export { ${Object.keys(versions).map((version) => version).join(", ")} };\n`
+fs.writeFileSync(indexFilePath, content);
