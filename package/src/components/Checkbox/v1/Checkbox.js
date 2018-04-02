3c34faec786b134875f95fc7cc92c3afa5286724
Nat Hamilton <info@nathamilton.com>
2018-04-02 14:32:55 -0500

    feat: checkbox working, added lodash method helper

--- package/src/components/Checkbox/v1/Checkbox.js
+++ package/src/components/Checkbox/v1/Checkbox.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import uniqueId from "lodash.uniqueid";
 import styled from "styled-components";
 import { applyTheme } from "helpers";
 
@@ -9,16 +10,97 @@ const StyledDiv = styled.div`
 
 class Checkbox extends Component {
   static propTypes = {
-
+    className: PropTypes.string,
+    isReadOnly: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
+    label: PropTypes.string.isRequired,
+    name: PropTypes.string,
+    onChange: PropTypes.func,
+    onChanging: PropTypes.func,
+    value: PropTypes.bool
   };
 
   static defaultProps = {
-
+    className: undefined,
+    isReadOnly: false,
+    name: undefined,
+    onChange() {},
+    onChanging() {},
+    value: undefined
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: uniqueId("Checkbox_"),
+      value: props.value || false
+    };
+  }
+
+  componentWillMount() {
+    this.handleChange(this.props.value || false);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { value } = this.props;
+    const { value: nextValue } = nextProps;
+
+    // Whenever a changed value prop comes in, we reset state to that, thus becoming clean.
+    if (value !== nextValue) {
+      this.setState({ value: nextValue || false });
+      this.handleChange(nextValue || false);
+    }
+  }
+
+  onChange = (event) => {
+    this.setValue(event.target.checked);
+  };
+
+  getValue() {
+    return this.state.value;
+  }
+
+  setValue(value) {
+    this.setState({ value });
+    this.handleChange(value);
+  }
+
+  resetValue() {
+    this.setValue(this.props.value || false);
+  }
+
+  handleChange(checked) {
+    if (this.lastValue === checked) return;
+    this.lastValue = checked;
+    const { onChanging, onChange } = this.props;
+    onChanging(checked);
+    onChange(checked);
+  }
+
+  // Input is dirty if value prop doesn't match value state. Whenever a changed
+  // value prop comes in, we reset state to that, thus becoming clean.
+  isDirty() {
+    return this.state.value !== this.props.value;
+  }
+
   render() {
+    const { className, isReadOnly, label } = this.props;
+    const { id, value } = this.state;
+
     return (
-      <StyledDiv>TEST</StyledDiv>
+      <div className={className}>
+        <label htmlFor={id}>
+          <input
+            checked={value === true}
+            id={id}
+            onChange={this.onChange}
+            readOnly={isReadOnly}
+            type="checkbox"
+            value="true"
+          />
+          {label}
+        </label>
+      </div>
     );
   }
 }
--- package/src/components/Checkbox/v1/Checkbox.md
+++ package/src/components/Checkbox/v1/Checkbox.md
@@ -1,3 +1,9 @@
 ### Checkbox
 
+```jsx
+<Field name="example">
+  <Checkbox name="example" label="Example Checkbox" />
+</Field>
+```
+
 Document component here. See https://react-styleguidist.js.org/docs/documenting.html
