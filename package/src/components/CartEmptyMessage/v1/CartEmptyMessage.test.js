d2ffb4a1daf7c4127b34fdf0a02c9fe53826cb52
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-06-20 15:22:56 -0700

    add tests

--- package/src/components/CartEmptyMessage/v1/CartEmptyMessage.test.js
+++ package/src/components/CartEmptyMessage/v1/CartEmptyMessage.test.js
@@ -2,9 +2,11 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
 import CartEmptyMessage from "./CartEmptyMessage";
+import Button from "../../Button/v1";
 
 test("basic snapshot", () => {
-  const component = renderer.create(<CartEmptyMessage />);
+  const onClick = () => {};
+  const component = renderer.create(<CartEmptyMessage components={{ContinueShoppingButton: Button}} onClick={onClick} />);
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
