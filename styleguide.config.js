8b76366b28be204c41ef95eb94f3094312b65fbb
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-06-21 13:22:38 -0700

    add checkout button to nav

--- styleguide.config.js
+++ styleguide.config.js
@@ -98,7 +98,7 @@ module.exports = {
           name: "Product"
         }),
         generateSection({
-          componentNames: ["CartSummary"],
+          componentNames: ["CartCheckoutButton", "CartSummary"],
           content: "styleguide/src/sections/Cart.md",
           name: "Cart"
         }),
