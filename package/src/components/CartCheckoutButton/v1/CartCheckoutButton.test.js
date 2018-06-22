52369ee83f7c0cc0c20bb920de87b035293dd028
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-06-22 12:03:39 -0700

    add test for click handling

--- package/src/components/CartCheckoutButton/v1/CartCheckoutButton.test.js
+++ package/src/components/CartCheckoutButton/v1/CartCheckoutButton.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 import Button from "../../Button/v1";
 import CartCheckoutButton from "./CartCheckoutButton";
 
@@ -34,4 +35,22 @@ test("cart checkout button with custom text", () => {
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+
+test("cart checkout button onClick", () => {
+  const isDisabled = false;
+  const testClick = jest.fn();
+
+  const component = shallow((
+    <CartCheckoutButton
+      buttonText="Custom button text"
+      components={{ Button }}
+      isDisabled={isDisabled}
+      onClick={testClick}
+    />
+  ));
+
+  component.simulate("click");
+  expect(testClick).toHaveBeenCalled();
 });
