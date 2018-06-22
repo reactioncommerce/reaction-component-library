772475da252d0dea549248502af570f0a051258a
Nat Hamilton <info@nathamilton.com>
2018-06-22 15:49:36 -0500

    feat: working out CartItem mobile layout

--- package/src/components/CartItem/v1/CartItem.js
+++ package/src/components/CartItem/v1/CartItem.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import Button from "../../Button/v1";
 import { applyTheme } from "../../../utils";
 
 const Item = styled.div`
-  border-bottom: solid 1px #f5f5f5;
+  border-bottom: solid 1px ${applyTheme("color_black05")};
   box-sizing: border-box;
   display: flex;
   padding: 1rem;
@@ -14,16 +13,44 @@ const Item = styled.div`
 
 const ItemContent = styled.div`
   background-color: rgba(145, 234, 86, 0.2);
+  display: flex;
   margin-left: 1rem;
   width: 100%;
 `;
 
 const ItemContentDetail = styled.div`
   background-color: rgba(45, 24, 86, 0.2);
+  display: flex;
+  flex: 1 1 100%;
+  flex-direction: column;
 `;
 
-const ItemContentMaybe = styled.div`
-  background-color: rgba(45, 24, 86, 0.2);
+const ItemContentDetailInner = styled.div`
+  background-color: rgba(134, 200, 200, 0.2);
+  flex: 1 1 auto;
+`;
+
+const ItemContentPrice = styled.div`
+  background-color: rgba(5, 224, 186, 0.2);
+  align-self: flex-end;
+  flex: 0 1 auto;
+`;
+
+const ItemRemoveButton = styled.button`
+  background-color: transparent;
+  border: none;
+  color: ${applyTheme("color_coolGrey400")};
+  cursor: pointer;
+  display: table;
+  flex: 0 1 auto;
+  font-family: ${applyTheme("font_family")};
+  font-size: ${applyTheme("font_size_small")};
+  font-weight: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  margin: 0.5rem 0 0;
+  padding: 0;
+  width: auto;
 `;
 
 class CartItem extends Component {
@@ -32,7 +59,8 @@ class CartItem extends Component {
       CartItemDetailComponent: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
       CartItemStockWarningComponent: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
       CartItemPriceComponent: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-      CartItemQuantityInputComponent: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
+      CartItemQuantityInputComponent: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+      CartItemRemoveButtonComponent: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
     }),
     item: PropTypes.shape({}),
     onChangeCartItemQuantity: PropTypes.func,
@@ -62,16 +90,23 @@ class CartItem extends Component {
     return (
       <Item>
         <img src="http://placehold.it/100" />
+
         <ItemContent>
           <ItemContentDetail>
-            <CartItemDetailComponent />
-            <CartItemStockWarningComponent inventoryQuantity={1} isLowInventoryQuantity={10} />
-            <CartItemQuantityInputComponent />
+            <ItemContentDetailInner>
+              <CartItemDetailComponent />
+
+              <CartItemStockWarningComponent inventoryQuantity={1} isLowInventoryQuantity={10} />
+
+              <CartItemQuantityInputComponent />
+            </ItemContentDetailInner>
+
+            <ItemRemoveButton onClick={this.handleRemoveItemFromCart}>Remove</ItemRemoveButton>
           </ItemContentDetail>
-          <Button isShortHeight isTextOnly onClick={this.handleRemoveItemFromCart}>
-            Remove
-          </Button>
-          <CartItemPriceComponent displayPrice="$20.00" />
+
+          <ItemContentPrice>
+            <CartItemPriceComponent displayPrice="$20.00" displayCompareAtPrice="$35.00" />
+          </ItemContentPrice>
         </ItemContent>
       </Item>
     );
