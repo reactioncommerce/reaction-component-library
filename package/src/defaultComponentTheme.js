952456cc455758ef9432c2fec97435a783631135
Nat Hamilton <info@nathamilton.com>
2018-06-25 15:58:38 -0500

    feat: updated defaultTheme

--- package/src/defaultComponentTheme.js
+++ package/src/defaultComponentTheme.js
@@ -159,6 +159,7 @@ const defaultStyles = {
   rui_color_black02: black02,
   rui_color_black05: black05,
   rui_color_black30: black30,
+  rui_color_black65: black65,
   rui_color_error: red,
   rui_color_stockWarning: red,
   rui_color_success: teal,
