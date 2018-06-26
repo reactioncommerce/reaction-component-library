7d2d52e65c10434d08899d265db0e958cb7f940a
Nat Hamilton <info@nathamilton.com>
2018-06-26 14:33:31 -0500

    feat: basic quantityInput ported

--- package/src/components/QuantityInput/v1/QuantityInput.js
+++ package/src/components/QuantityInput/v1/QuantityInput.js
@@ -9,17 +9,23 @@ import { applyTheme } from "../../../utils";
 const styles = (theme) => ({
   incrementButton: {
     backgroundColor: "#fafafa",
+    boxSizing: "border-box",
     color: "#3c3c3c",
-    fontSize: "12px",
+    fontSize: "1rem",
+    lineHieght: 1,
     padding: 6
   },
   quantityContainer: {
     padding: 0,
     border: `1px solid #d9d9d9`,
+    boxSizing: "border-box",
     backgroundColor: theme.palette.common.white,
     borderRadius: "2px"
   },
   quantityInput: {
+    borderLeft: `1px solid #d9d9d9`,
+    borderRight: `1px solid #d9d9d9`,
+    boxSizing: "border-box",
     color: "#3c3c3c",
     fontSize: "12px",
     width: "40px",
@@ -27,9 +33,7 @@ const styles = (theme) => ({
     "&:focus": {
       borderColor: "#80bdff",
       boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)"
-    },
-    borderLeft: `1px solid #d9d9d9`,
-    borderRight: `1px solid #d9d9d9`
+    }
   }
 });
 
@@ -38,12 +42,50 @@ class QuantityInput extends Component {
 
   static defaultProps = {};
 
+  constructor(props) {
+    super(props);
+
+    const value = props.value || 0;
+
+    this.state = {
+      initialValue: props.value,
+      value
+    };
+  }
+
+  handleQuantityInputChange = (event) => {
+    const { value } = event.target;
+
+    const numericValue = Math.floor(Number(value));
+
+    if (Number.isNaN(numericValue)) {
+      return null;
+    }
+
+    return this.setState({ value: numericValue });
+  };
+
+  handleIncrementButton = () => {
+    const value = this.state.value + 1;
+
+    this.setState({ value });
+  };
+
+  handleDecrementButton = () => {
+    const value = this.state.value - 1;
+
+    if (value >= 0) {
+      this.setState({ value });
+    }
+  };
+
   render() {
     const { classes: { incrementButton, quantityContainer, quantityInput } } = this.props;
+    const { value } = this.state;
     return (
       <TextField
         id="addToCartQuantityInput"
-        value={2}
+        value={value}
         onChange={this.handleQuantityInputChange}
         InputProps={{
           startAdornment: (
@@ -54,7 +96,7 @@ class QuantityInput extends Component {
                 onClick={this.handleDecrementButton}
                 className={incrementButton}
               >
-                -
+                <i className="fa fa-minus" />
               </ButtonBase>
             </InputAdornment>
           ),
@@ -66,7 +108,7 @@ class QuantityInput extends Component {
                 onClick={this.handleIncrementButton}
                 className={incrementButton}
               >
-                +
+                <i className="fa fa-plus" />
               </ButtonBase>
             </InputAdornment>
           ),
