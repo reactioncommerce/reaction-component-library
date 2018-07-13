ea99186de7df7e892b191e4228e1073cd3750093
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-07-13 12:04:50 -0600

    test: add tests for onClick

--- package/src/components/CheckoutActionComplete/v1/CheckoutActionComplete.test.js
+++ package/src/components/CheckoutActionComplete/v1/CheckoutActionComplete.test.js
@@ -1,11 +1,35 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
+import Button from "../../Button/v1";
 import CheckoutActionComplete from "./CheckoutActionComplete";
 
+const fakeEvent = { preventDefault() {} };
+
 test("basic snapshot", () => {
-  const component = renderer.create(<CheckoutActionComplete />);
+  const onClick = () => {};
+
+  const Address = "<div><p>123 Main Street</p><p>Anytown, USA 01776</p></div>";
+
+  const component = renderer.create(
+    <CheckoutActionComplete
+      components={{ChangeButton: Button}}
+      label="Shipping address"
+      content={Address}
+      onChange={onClick}
+    />
+  );
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test("calls onClick when clicked", () => {
+  const clickSpy = jest.fn();
+
+  const item = shallow(<Button onClick={clickSpy} />);
+
+  item.simulate("click", fakeEvent);
+
+  expect(clickSpy).toHaveBeenCalled();
 });
