aed21f713939ee0ed961847bddf13a9bc13d048e
Erik Kieckhafer <kieckhafer@users.noreply.github.com>
2018-07-13 08:58:37 -0600

    lint fix

--- package/src/components/CheckoutTopHat/v1/CheckoutTopHat.test.js
+++ package/src/components/CheckoutTopHat/v1/CheckoutTopHat.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { shallow } from "enzyme";
 import CheckoutTopHat from "./CheckoutTopHat";
 
 test("render Top Hat with message", () => {
