7e1b68a04b0feaa9a96d06789ec95ec07dc3a063
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-07-13 08:43:13 -0600

    add TopHat component code

--- package/src/components/CheckoutTopHat/v1/CheckoutTopHat.js
+++ package/src/components/CheckoutTopHat/v1/CheckoutTopHat.js
@@ -3,23 +3,39 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { applyTheme } from "../../../utils";
 
-const StyledDiv = styled.div`
-  color: #333333;
+const TopHatContainer = styled.div`
+  background-color: ${applyTheme("color_black05")};
+  display: flex;
+  height: 30px;
+  justify-content: center;
+  width: 100%;
+`;
+
+const TopHatMessage = styled.div`
+  align-items: center;
+  color: ${applyTheme("color_coolGrey500")};
+  display: flex;
+  font-size: 12px;
+  font-weight: ${applyTheme("font_weight_bold")};
 `;
 
 class CheckoutTopHat extends Component {
   static propTypes = {
-
-  };
-
-  static defaultProps = {
-
+    checkoutMessage: PropTypes.string
   };
 
   render() {
-    return (
-      <StyledDiv>TEST</StyledDiv>
-    );
+    const { checkoutMessage } = this.props;
+
+    if (checkoutMessage) {
+      return (
+        <TopHatContainer>
+          <TopHatMessage>{checkoutMessage}</TopHatMessage>
+        </TopHatContainer>
+      );
+    }
+
+    return null;
   }
 }
 
