0514616118c07d82f6938f8f363b62089a19b146
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-07-17 15:50:22 -0700

    test: add tests for CheckoutEmailAddress component

--- package/src/components/CheckoutEmailAddress/v1/CheckoutEmailAddress.test.js
+++ package/src/components/CheckoutEmailAddress/v1/CheckoutEmailAddress.test.js
@@ -3,8 +3,15 @@ import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
 import CheckoutEmailAddress from "./CheckoutEmailAddress";
 
-test("basic snapshot", () => {
-  const component = renderer.create(<CheckoutEmailAddress />);
+test("render email address of a user with an account", () => {
+  const component = shallow(<CheckoutEmailAddress emailAddress="account.email@example.com" isAccountEmail={true} />);
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("render email address of a guest", () => {
+  const component = shallow(<CheckoutEmailAddress emailAddress="guest.email@example.com" isAccountEmail={false} />);
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
