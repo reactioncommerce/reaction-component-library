acbcce44b98d49f773aba885ab7a851f5c8ccc0f
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-07-17 15:57:12 -0700

    test: update tests

--- package/src/components/CheckoutActionComplete/v1/CheckoutActionComplete.test.js
+++ package/src/components/CheckoutActionComplete/v1/CheckoutActionComplete.test.js
@@ -11,13 +11,15 @@ test("basic snapshot", () => {
 
   const Address = "<div><p>123 Main Street</p><p>Anytown, USA 01776</p></div>";
 
-  const component = renderer.create(<CheckoutActionComplete
-    components={{ ChangeButton: Button }}
-    label="Shipping address"
-    content={Address}
-    onClickChangeButton={onClick}
-    stepNumber={2}
-  />);
+  const component = renderer.create((
+    <CheckoutActionComplete
+      components={{ ChangeButton: Button }}
+      label="Shipping address"
+      content={Address}
+      onClickChangeButton={onClick}
+      stepNumber={2}
+    />
+  ));
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
--- package/src/components/CheckoutEmailAddress/v1/CheckoutEmailAddress.test.js
+++ package/src/components/CheckoutEmailAddress/v1/CheckoutEmailAddress.test.js
@@ -1,17 +1,26 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { shallow } from "enzyme";
 import CheckoutEmailAddress from "./CheckoutEmailAddress";
 
 test("render email address of a user with an account", () => {
-  const component = shallow(<CheckoutEmailAddress emailAddress="account.email@example.com" isAccountEmail={true} />);
+  const component = renderer.create((
+    <CheckoutEmailAddress
+      emailAddress="account.email@example.com"
+      isAccountEmail={true}
+    />
+  ));
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test("render email address of a guest", () => {
-  const component = shallow(<CheckoutEmailAddress emailAddress="guest.email@example.com" isAccountEmail={false} />);
+  const component = renderer.create((
+    <CheckoutEmailAddress
+      emailAddress="guest.email@example.com"
+      isAccountEmail={false}
+    />
+  ));
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
