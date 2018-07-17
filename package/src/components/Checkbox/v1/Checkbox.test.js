ffd02a741113ba7db18243aa42f88fa61a0b726d
Machiko Yasuda <machiko@reactioncommerce.com>
2018-07-17 15:41:25 -0700

    test: update snapshot; add disabled test

--- package/src/components/Checkbox/v1/Checkbox.test.js
+++ package/src/components/Checkbox/v1/Checkbox.test.js
@@ -2,8 +2,15 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Checkbox from "./Checkbox";
 
-test("basic snapshot", () => {
-  const component = renderer.create(<Checkbox />);
+test("renders with props", () => {
+  const component = renderer.create(<Checkbox label="Label" />);
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("renders disabled", () => {
+  const component = renderer.create(<Checkbox label="Disabled" isReadOnly />);
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
