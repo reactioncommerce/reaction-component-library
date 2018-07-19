977cd462ed628ebb7b5ce30a047087374e878aef
Nat Hamilton <info@nathamilton.com>
2018-07-19 10:05:15 -0500

    chore: added reacto-form as dep/peerDep, uodated PhoneNumberInput docs

--- package/src/components/PhoneNumberInput/v1/PhoneNumberInput.js
+++ package/src/components/PhoneNumberInput/v1/PhoneNumberInput.js
@@ -3,7 +3,80 @@ import PropTypes from "prop-types";
 import TextInput from "../../TextInput/v1";
 
 class PhoneNumberInput extends Component {
-  static propTypes = {};
+  static propTypes = {
+    /**
+     * Custom class name
+     */
+    className: PropTypes.string,
+    /**
+     * An array of validation errors
+     */
+    errors: PropTypes.array,
+    /**
+     * Enable when a input's value has been validated
+     */
+    hasBeenValidated: PropTypes.bool,
+    /**
+     * Add an icon to the input by passing an icon node,
+     */
+    icon: PropTypes.node,
+    /**
+     * Add extra context to your icon for assistive technologies
+     */
+    iconAccessibilityText: PropTypes.string,
+    /**
+     * Overwrite the default clear input icon by passing an icon node
+     */
+    iconClear: PropTypes.node,
+    /**
+     * Overwrite the default clear input icon accessibilty text
+     */
+    iconClearAccessibilityText: PropTypes.string,
+    /**
+     * Overwrite the default error input icon by passing an icon node
+     */
+    iconError: PropTypes.node,
+    /**
+     * Overwrite the default valid input icon by passing an icon node
+     */
+    iconValid: PropTypes.node,
+    /**
+     * Enable when using the input on a dark background, disabled by default
+     */
+    isOnDarkBackground: PropTypes.bool,
+    /**
+     * Enable to make the input read only / disabled, disabled by default
+     */
+    isReadOnly: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
+    /**
+     * Input name
+     */
+    name: PropTypes.string,
+    /**
+     * On change hanlder for input
+     */
+    onChange: PropTypes.func,
+    /**
+     * On changing hanlder for input
+     */
+    onChanging: PropTypes.func,
+    /**
+     * Custome click hanlder for added icon
+     */
+    onIconClick: PropTypes.func,
+    /**
+     * On submit hanlder for input
+     */
+    onSubmit: PropTypes.func,
+    /**
+     * Input placeholder text
+     */
+    placeholder: PropTypes.string,
+    /**
+     * Prepopulate the input's value
+     */
+    value: PropTypes.string
+  };
 
   static defaultProps = {};
 
@@ -18,8 +91,9 @@ class PhoneNumberInput extends Component {
   };
 
   render() {
+    const { ...props } = this.props;
     const { value } = this.state;
-    return <TextInput onChanging={this.handdleChanging} value={value} />;
+    return <TextInput onChanging={this.handdleChanging} value={value} {...props} />;
   }
 }
 
--- package/src/components/PhoneNumberInput/v1/PhoneNumberInput.md
+++ package/src/components/PhoneNumberInput/v1/PhoneNumberInput.md
@@ -1,6 +1,10 @@
 ### PhoneNumberInput
 
+The `PhoneNumberInput` is a swallow wrapper of the `TextInput` that removes non-digit characters from the value. 
+
+*Example:* (504) 393-7303 => 5043937303
+
 #### Basic Usage
 ```jsx
-<PhoneNumberInput />
+<PhoneNumberInput placeholder="Phone Number" />
 ```
