309e067d94269b240a3b257c83ae58a19e5d9316
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-07-19 13:28:39 -0700

    test: add tests for different statuses

--- package/src/components/CheckoutAction/v1/CheckoutAction.test.js
+++ package/src/components/CheckoutAction/v1/CheckoutAction.test.js
@@ -1,10 +1,116 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { shallow } from "enzyme";
+import Button from "../../Button/v1";
+import CheckoutActionComplete from "../../CheckoutActionComplete/v1";
+import CheckoutActionIncomplete from "../../CheckoutActionIncomplete/v1";
 import CheckoutAction from "./CheckoutAction";
 
-test("basic snapshot", () => {
-  const component = renderer.create(<CheckoutAction />);
+test("CheckoutAction with `active` status", () => {
+  const cart = {};
+  const isLoading = false;
+  const onClick = () => {};
+  const Address = (
+    <div>
+      Ms. Jane Doe<br />
+      123 Main Street<br />
+      Anytown, USA 01776
+    </div>
+  );
+
+  const component = renderer.create((
+    <CheckoutAction
+      ActiveStepComponent="Placeholder: active step isn't yet available"
+      CompleteStepComponent={
+        <CheckoutActionComplete
+          components={{ ChangeButton: Button }}
+          content={Address}
+          onClickChangeButton={onClick}
+        />
+      }
+      IncompleteStepComponent={
+        <CheckoutActionIncomplete />
+      }
+      cart={cart}
+      isLoading={isLoading}
+      label="Shipping address"
+      status="active"
+      stepNumber={2}
+    />
+  ));
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("CheckoutAction with `complete` status", () => {
+  const cart = {};
+  const isLoading = false;
+  const onClick = () => {};
+  const Address = (
+    <div>
+      Ms. Jane Doe<br />
+      123 Main Street<br />
+      Anytown, USA 01776
+    </div>
+  );
+
+  const component = renderer.create((
+    <CheckoutAction
+      ActiveStepComponent="Placeholder: active step isn't yet available"
+      CompleteStepComponent={
+        <CheckoutActionComplete
+          components={{ ChangeButton: Button }}
+          content={Address}
+          onClickChangeButton={onClick}
+        />
+      }
+      IncompleteStepComponent={
+        <CheckoutActionIncomplete />
+      }
+      cart={cart}
+      isLoading={isLoading}
+      label="Shipping address"
+      status="complete"
+      stepNumber={2}
+    />
+  ));
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("CheckoutAction with `incomplete` status", () => {
+  const cart = {};
+  const isLoading = false;
+  const onClick = () => {};
+  const Address = (
+    <div>
+      Ms. Jane Doe<br />
+      123 Main Street<br />
+      Anytown, USA 01776
+    </div>
+  );
+
+  const component = renderer.create((
+    <CheckoutAction
+      ActiveStepComponent="Placeholder: active step isn't yet available"
+      CompleteStepComponent={
+        <CheckoutActionComplete
+          components={{ ChangeButton: Button }}
+          content={Address}
+          onClickChangeButton={onClick}
+        />
+      }
+      IncompleteStepComponent={
+        <CheckoutActionIncomplete />
+      }
+      cart={cart}
+      isLoading={isLoading}
+      label="Shipping address"
+      status="incomplete"
+      stepNumber={2}
+    />
+  ));
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
