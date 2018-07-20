c4837ef4eac5f695ce54c539dbbd41ed4a93d4af
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-07-20 09:36:22 -0700

    test: update tests for new code from component

--- package/src/components/CheckoutAction/v1/CheckoutAction.test.js
+++ package/src/components/CheckoutAction/v1/CheckoutAction.test.js
@@ -19,15 +19,15 @@ test("CheckoutAction with `active` status", () => {
 
   const component = renderer.create((
     <CheckoutAction
-      ActiveStepComponent="Placeholder: active step isn't yet available"
-      CompleteStepComponent={
+      activeStepElement="Placeholder: active step isn't yet available"
+      completeStepElement={
         <CheckoutActionComplete
           components={{ ChangeButton: Button }}
           content={Address}
           onClickChangeButton={onClick}
         />
       }
-      IncompleteStepComponent={
+      incompleteStepElement={
         <CheckoutActionIncomplete />
       }
       cart={cart}
@@ -56,15 +56,15 @@ test("CheckoutAction with `complete` status", () => {
 
   const component = renderer.create((
     <CheckoutAction
-      ActiveStepComponent="Placeholder: active step isn't yet available"
-      CompleteStepComponent={
+      activeStepElement="Placeholder: active step isn't yet available"
+      completeStepElement={
         <CheckoutActionComplete
           components={{ ChangeButton: Button }}
           content={Address}
           onClickChangeButton={onClick}
         />
       }
-      IncompleteStepComponent={
+      incompleteStepElement={
         <CheckoutActionIncomplete />
       }
       cart={cart}
@@ -93,21 +93,97 @@ test("CheckoutAction with `incomplete` status", () => {
 
   const component = renderer.create((
     <CheckoutAction
-      ActiveStepComponent="Placeholder: active step isn't yet available"
-      CompleteStepComponent={
+      activeStepElement="Placeholder: active step isn't yet available"
+      completeStepElement={
         <CheckoutActionComplete
           components={{ ChangeButton: Button }}
           content={Address}
           onClickChangeButton={onClick}
         />
       }
-      IncompleteStepComponent={
+      incompleteStepElement={
         <CheckoutActionIncomplete />
       }
       cart={cart}
       isLoading={isLoading}
       label="Shipping address"
       status="incomplete"
+      stepNumber={2}
+    />
+  ));
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("CheckoutAction with `complete` status and label override via props", () => {
+  const cart = {};
+  const isLoading = false;
+  const onClick = () => {};
+  const Address = (
+    <div>
+      Ms. Jane Doe<br />
+      123 Main Street<br />
+      Anytown, USA 01776
+    </div>
+  );
+
+  const component = renderer.create((
+    <CheckoutAction
+      activeStepElement="Placeholder: active step isn't yet available"
+      completeStepElement={
+        <CheckoutActionComplete
+          components={{ ChangeButton: Button }}
+          content={Address}
+          label="Label override"
+          onClickChangeButton={onClick}
+        />
+      }
+      incompleteStepElement={
+        <CheckoutActionIncomplete />
+      }
+      cart={cart}
+      isLoading={isLoading}
+      label="Shipping address"
+      status="complete"
+      stepNumber={2}
+    />
+  ));
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("CheckoutAction with `complete` status and stepNumber override via props", () => {
+  const cart = {};
+  const isLoading = false;
+  const onClick = () => {};
+  const Address = (
+    <div>
+      Ms. Jane Doe<br />
+      123 Main Street<br />
+      Anytown, USA 01776
+    </div>
+  );
+
+  const component = renderer.create((
+    <CheckoutAction
+      activeStepElement="Placeholder: active step isn't yet available"
+      completeStepElement={
+        <CheckoutActionComplete
+          components={{ ChangeButton: Button }}
+          content={Address}
+          onClickChangeButton={onClick}
+          stepNumber={500}
+        />
+      }
+      incompleteStepElement={
+        <CheckoutActionIncomplete />
+      }
+      cart={cart}
+      isLoading={isLoading}
+      label="Shipping address"
+      status="complete"
       stepNumber={2}
     />
   ));
