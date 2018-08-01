6686f2d61c924d264eaf2f753cd75f485a977c27
Machiko Yasuda <machiko@reactioncommerce.com>
2018-07-31 17:25:10 -0700

    chore: lint

--- package/src/components/Checkbox/v1/Checkbox.js
+++ package/src/components/Checkbox/v1/Checkbox.js
@@ -8,7 +8,13 @@ const StyledDiv = styled.div`
   margin-bottom: ${applyTheme("checkboxVerticalSpacing")};
 `;
 
+/* eslint-disable max-len */
+/* eslint-disable quotes */
+
 const checkboxIcon = `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z'></path></svg>`;
+
+/* eslint-enable quotes */
+/* eslint-enable max-len */
 
 const StyledInput = styled.input`
   opacity: 0;
@@ -98,7 +104,9 @@ class Checkbox extends Component {
     /**
      * True for a checked checkbox, undefined for an unchecked checkbox
      */
+    /* eslint-disable react/boolean-prop-naming */
     value: PropTypes.bool
+    /* eslint-enable react/boolean-prop-naming */
   };
 
   static defaultProps = {
