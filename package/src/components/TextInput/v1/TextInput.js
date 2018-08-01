18a385bef0da92169fcf9a3fb765a48f2a453d78
Machiko Yasuda <machiko@reactioncommerce.com>
2018-07-31 17:26:41 -0700

    chore: lint

--- package/src/components/TextInput/v1/TextInput.js
+++ package/src/components/TextInput/v1/TextInput.js
@@ -109,11 +109,6 @@ const IconWrapper = styled.div`
   }
 `;
 
-const FontIcon = styled.i`
-  font-size: 1em;
-  vertical-align: middle;
-`;
-
 const ClearButton = styled.div`
   background-color: transparent;
   border: none;
@@ -174,6 +169,7 @@ const TextareaClearButton = styled.div`
 /* eslint-disable max-len */
 
 const defaultClearIcon = (
+  // credit: https://fontawesome.com/icons/times-circle?style=regular
   <svg
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
@@ -185,8 +181,7 @@ const defaultClearIcon = (
 );
 
 const defaultValidIcon = (
-  // check-circle regular, FontAwesome v5
-  // https://fontawesome.com/icons/check-circle?style=regular
+  // credit: https://fontawesome.com/icons/check-circle?style=regular
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 512 512"
@@ -194,13 +189,13 @@ const defaultValidIcon = (
   >
     <path
       fill="currentColor"
-      d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z" />
+      d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"
+    />
   </svg>
 );
 
 const defaultInvalidIcon = (
-  // exclamation-triangle, FontAwesome v5
-  // from https://fontawesome.com/icons/exclamation-triangle?style=solid
+  // credit: https://fontawesome.com/icons/exclamation-triangle?style=solid
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 576 512"
@@ -208,7 +203,8 @@ const defaultInvalidIcon = (
   >
     <path
       fill="currentColor"
-      d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z" />
+      d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"
+    />
   </svg>
 );
 
