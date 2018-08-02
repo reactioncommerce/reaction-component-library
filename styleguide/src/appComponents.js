16d51584a07de09f71abe18ddf6f70e99c639701
Machiko Yasuda <machiko@reactioncommerce.com>
2018-08-02 10:34:49 -0700

    refactor(textinput): add iconValid to appComponents

--- styleguide/src/appComponents.js
+++ styleguide/src/appComponents.js
@@ -22,7 +22,9 @@ const FontIcon = styled.i`
 `;
 
 /* eslint-disable max-len */
+
 const iconClear = (
+  // credit: https://fontawesome.com/icons/times-circle?style=regular
   <svg
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
@@ -46,6 +48,20 @@ const iconError = (
   </svg>
 );
 
+const iconValid = (
+  // credit: https://fontawesome.com/icons/check-circle?style=regular
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    style={{ height: "0.875em", verticalAlign: "middle" }}
+  >
+    <path
+      fill="currentColor"
+      d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"
+    />
+  </svg>
+);
+
 /* eslint-enable max-len */
 
 export default {
@@ -58,7 +74,7 @@ export default {
   Field,
   iconClear,
   iconError,
-  iconValid: (<FontIcon className="far fa-check-circle" />),
+  iconValid,
   MiniCartSummary,
   PhoneNumberInput,
   Price,
