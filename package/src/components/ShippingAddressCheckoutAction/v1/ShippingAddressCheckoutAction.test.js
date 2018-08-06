1316a2100fb2b0f03525bbc368a55e2b59a4feaa
Nat Hamilton <info@nathamilton.com>
2018-08-06 12:20:23 -0500

    test: updated snapshots

--- package/src/components/ShippingAddressCheckoutAction/v1/ShippingAddressCheckoutAction.test.js
+++ package/src/components/ShippingAddressCheckoutAction/v1/ShippingAddressCheckoutAction.test.js
@@ -1,10 +1,39 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import mockComponents from "../../../tests/mockComponents";
 import ShippingAddressCheckoutAction from "./ShippingAddressCheckoutAction";
 
-test("basic snapshot", () => {
-  const component = renderer.create(<ShippingAddressCheckoutAction />);
+test("basic snapshot with empty address", () => {
+  const component = renderer.create(
+    <ShippingAddressCheckoutAction
+      components={mockComponents}
+      fullfillmentGroup={{ data: { shippingAddress: null } }}
+    />
+  );
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+// test("basic snapshot with address", () => {
+//   const address = {
+//     address1: "7742 Hwy 23",
+//     address2: "",
+//     country: "US",
+//     city: "Belle Chasse",
+//     firstName: "Salvos",
+//     lastName: "Seafood",
+//     postal: "70037",
+//     region: "LA",
+//     phone: "(504) 393-7303"
+//   };
+//   const component = renderer.create(
+//     <ShippingAddressCheckoutAction
+//       components={mockComponents}
+//       fullfillmentGroup={{ data: { shippingAddress: address } }}
+//     />
+//   );
+
+//   const tree = component.toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
