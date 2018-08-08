9fd1b76a65e03c95b3301fc58db2e1b50c33ace1
Machiko Yasuda <machiko@reactioncommerce.com>
2018-08-08 11:55:30 -0700

    style(selectableitem): first pass at styling radio button

--- package/src/components/SelectableItem/v1/SelectableItem.js
+++ package/src/components/SelectableItem/v1/SelectableItem.js
@@ -1,24 +1,79 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import styled from "styled-components";
-import { applyTheme } from "../../../utils";
+// import { applyTheme } from "../../../utils";
 
-const StyledDiv = styled.div`
+const StyledItem = styled.div`
   color: #333333;
+  input {
+    cursor: pointer;
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+    visibility: visible;
+    white-space: nowrap;
+  }
+  input:checked + label span::before {
+    content: " ";
+    display: inline-block;
+    position: relative;
+    width: 11px;
+    height: 11px;
+    border-radius: 50%;
+    background-color: #505558;
+   }
+  label {
+    font-family: Source Sans Pro;
+    color: #505558;
+    font-size: 14px;
+    letter-spacing: 0.3px;
+    line-height: 17.5px;
+    cursor: pointer;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-align: center;
+    align-items: center;
+  }
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-color: #505558;
+    background-color: white;
+    border: 2px solid #505558;
+    height: 20px;
+    width: 20px;
+    margin-right: 10px;
+    border-radius: 50%;
+    box-sizing: border-box;
+  }
 `;
 
 class SelectableItem extends Component {
-  static propTypes = {
-
-  };
-
-  static defaultProps = {
-
-  };
-
   render() {
     return (
-      <StyledDiv>TEST</StyledDiv>
+      <StyledItem>
+        <input
+          id="radio-button-1"
+          className="bx--radio-button"
+          type="radio"
+          name="radio-button"
+          tabIndex="0"
+        />
+        <label
+          htmlFor="radio-button-1"
+          className="bx--radio-button-1"
+        >
+          <span />
+          One shot
+        </label>
+        {/* <span>Detail</span> */}
+      </StyledItem >
     );
   }
 }
--- package/src/components/SelectableItem/v1/SelectableItem.md
+++ package/src/components/SelectableItem/v1/SelectableItem.md
@@ -2,6 +2,10 @@
 
 `SelectableItems` in a `SelectableList` are used to select shipping methods, addresses and credit cards. Each item consists of a radio button, label and display value. The label and display value can both support text or render other elements, like icons and links.
 
+```jsx noeditor
+<SelectableItem/>
+```
+
 #### Usage
 - All labels should be written in sentence caps.
 - Radio buttons should be used when only one option can be selected from a set of defined values.
