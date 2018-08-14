d9c0246dc3642875a43dbcb74cba98e72a4f657a
Nat Hamilton <info@nathamilton.com>
2018-08-14 12:30:23 -0500

    test: updated CheckoutActions test and snapshot

--- package/src/components/CheckoutActions/v1/CheckoutActions.test.js
+++ package/src/components/CheckoutActions/v1/CheckoutActions.test.js
@@ -1,9 +1,24 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import mockComponents from "../../../tests/mockComponents";
 import CheckoutActions from "./CheckoutActions";
 
+class mockCheckoutAction extends React.Component {
+  static renderComplete = () => <span />;
+  render() {
+    return <span />;
+  }
+}
+
+const mockActions = [
+  {
+    label: "mock action one",
+    component: mockCheckoutAction
+  }
+];
+
 test("basic snapshot", () => {
-  const component = renderer.create(<CheckoutActions />);
+  const component = renderer.create(<CheckoutActions components={mockComponents} actions={mockActions} />);
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
