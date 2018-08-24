145b9f55a24d86a593463b317226e7753e07cf3c
Machiko Yasuda <machiko@reactioncommerce.com>
2018-08-24 14:33:23 -0700

    chore: remove spinner from utils

--- package/src/utils/index.js
+++ package/src/utils/index.js
@@ -2,7 +2,6 @@ export { default as applyTheme } from "./applyTheme";
 export { default as CustomPropTypes } from "./CustomPropTypes";
 export { default as getFromTheme } from "./getFromTheme";
 export { default as preventAccidentalDoubleClick } from "./preventAccidentalDoubleClick";
-export { default as spinner } from "./spinner";
 export { default as getRequiredValidator } from "./getRequiredValidator";
 export { default as getPhoneNumberValidator } from "./getPhoneNumberValidator";
 export { default as withStripeElements } from "./withStripeElements";
