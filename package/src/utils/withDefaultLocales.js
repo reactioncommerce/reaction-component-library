52fcb1a16abdbd3375ac04ef3d49f4549192b9e9
Nat Hamilton <info@nathamilton.com>
2018-08-29 16:30:31 -0500

    refactor: moved locales state to HOC and provide via props

--- package/src/utils/withDefaultLocales.js
+++ package/src/utils/withDefaultLocales.js
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import isEmpty from "lodash.isempty";
 
 export default function withDefaultLocales(WrappedComponent) {
   class WithDefaultLocales extends Component {
+    static propTypes = {
+      forwardRef: PropTypes.func,
+      locales: PropTypes.object
+    };
+
+    static defaultProps = {
+      locales: {}
+    };
+
+    state = {
+      locales: this.props.locales
+    };
+
+    async componentDidMount() {
+      let { locales } = this.state;
+      if (isEmpty(locales)) {
+        locales = await this.loadLocales();
+        this.setState({ locales });
+      }
+    }
+
     loadLocales = async () => {
       const locales = await import("./locales.json");
       return locales;
     };
 
     render() {
-      return <WrappedComponent ref={this.props.forwardRef} getDefaultLocales={this.loadLocales} {...this.props} />;
+      const { locales } = this.state;
+      return <WrappedComponent ref={this.props.forwardRef} {...this.props} locales={locales} />;
     }
   }
 
