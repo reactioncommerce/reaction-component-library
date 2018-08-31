291b10952b19733198bd30cf25bab00ca6c70c78
Nat Hamilton <info@nathamilton.com>
2018-08-31 14:05:53 -0500

    test: AddressBook now has mockContext

--- package/src/components/AddressBook/v1/AddressBook.test.js
+++ package/src/components/AddressBook/v1/AddressBook.test.js
@@ -1,10 +1,11 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { shallow } from "enzyme";
+// import { shallow } from "enzyme";
+import mockComponents from "../../../tests/mockComponents";
 import AddressBook from "./AddressBook";
 
 test("basic snapshot", () => {
-  const component = renderer.create(<AddressBook />);
+  const component = renderer.create(<AddressBook components={mockComponents} />);
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
