b7ab88c07cc5143404b7dbc93a6e6b35ab12b011
Nat Hamilton <info@nathamilton.com>
2018-09-19 09:19:57 -0500

    test: another ViewerInfo test for rendering without any props

--- package/src/components/ViewerInfo/v1/ViewerInfo.test.js
+++ package/src/components/ViewerInfo/v1/ViewerInfo.test.js
@@ -2,6 +2,13 @@ import React from "react";
 import renderer from "react-test-renderer";
 import ViewerInfo from "./ViewerInfo";
 
+test("Render a viewerinfo without any props", () => {
+  const component = renderer.create(<ViewerInfo />);
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
 test("Render a viewer's initials and first name", () => {
   const viewer = {
     firstName: "Issac",
