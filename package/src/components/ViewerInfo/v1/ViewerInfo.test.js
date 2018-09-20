eac4fdeefba812eaeffd52e96d72819895acf4fa
Nat Hamilton <info@nathamilton.com>
2018-09-20 13:19:34 -0500

    test: updated test to use the new props

--- package/src/components/ViewerInfo/v1/ViewerInfo.test.js
+++ package/src/components/ViewerInfo/v1/ViewerInfo.test.js
@@ -2,21 +2,50 @@ import React from "react";
 import renderer from "react-test-renderer";
 import ViewerInfo from "./ViewerInfo";
 
-test("Render a viewerinfo without any props", () => {
-  const component = renderer.create(<ViewerInfo />);
-
+test("Render with only required props", () => {
+  const mockViewer = {
+    primaryEmailAddress: "email@domain.com"
+  };
+  const component = renderer.create(<ViewerInfo viewer={mockViewer} />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-test("Render a viewer's initials and first name", () => {
-  const viewer = {
+test("Render with only email & first name props", () => {
+  const mockViewer = {
     firstName: "Issac",
-    lastName: "Newton"
+    primaryEmailAddress: "email@domain.com"
   };
+  const component = renderer.create(<ViewerInfo viewer={mockViewer} />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-  const component = renderer.create(<ViewerInfo viewer={viewer} />);
+test("Render with full name and email props", () => {
+  const mockViewer = {
+    firstName: "Issac",
+    lastName: "Newton",
+    primaryEmailAddress: "email@domain.com"
+  };
+  const component = renderer.create(<ViewerInfo viewer={mockViewer} />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
+test("Render with full prop", () => {
+  const mockViewer = {
+    primaryEmailAddress: "email@domain.com"
+  };
+  const component = renderer.create(<ViewerInfo viewer={mockViewer} full />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("Render with compact prop", () => {
+  const mockViewer = {
+    primaryEmailAddress: "email@domain.com"
+  };
+  const component = renderer.create(<ViewerInfo viewer={mockViewer} compact />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
