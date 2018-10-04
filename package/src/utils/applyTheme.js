c125e1b8e107f6a527298141e5ea80b9ec0d35de
Eric Dobbertin <eric@dairystatedesigns.com>
2018-10-04 16:29:21 -0500

    refactor: update applyTheme signature

--- package/src/utils/applyTheme.js
+++ package/src/utils/applyTheme.js
@@ -1,11 +1,5 @@
 import getFromTheme from "./getFromTheme";
 
-export default function applyTheme(themeProp, suffix) {
-  return (props) => {
-    let finalSuffix = "";
-    if (typeof suffix === "string" && suffix.length) {
-      finalSuffix = `_${suffix}`;
-    }
-    return getFromTheme(props, `rui_${themeProp}${finalSuffix}`);
-  };
+export default function applyTheme(themePath, group = "components") {
+  return (props) => getFromTheme(props, `rui_${group}.${themePath}`);
 }
