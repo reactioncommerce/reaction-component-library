937ddb81f7ac42b13d6863955b32246586a2a350
Nat Hamilton <info@nathamilton.com>
2018-10-05 10:37:20 -0500

    feat: started Accordion styles / props

--- package/src/components/Accordion/v1/Accordion.js
+++ package/src/components/Accordion/v1/Accordion.js
@@ -1,14 +1,62 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-// import { applyTheme } from "../../../utils";
+import { withComponents } from "@reactioncommerce/components-context";
+import { applyTheme, CustomPropTypes } from "../../../utils";
 
-const StyledDiv = styled.div`
+const duration = "250ms";
+const ease = "ease-in-out";
+const openTransition = `max-height ${duration} ${ease}, padding 0ms ${ease}`;
+const closeTransition = `max-height ${duration} ${ease}, padding 0ms ${ease} ${duration}`;
+
+const AccordionWrapper = styled.div`
+  box-sizing: border-box;
   color: inherit;
+  &:first-of-type {
+    & > div {
+      background-color: red;
+    }
+  }
+`;
+
+const AccordionHeader = styled.div`
+  border-color: ${applyTheme("accordionBorderColor")};
+  border-style: ${applyTheme("accordionBorderStyle")};
+  border-width: ${applyTheme("accordionBorderWidth")};
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  padding: ${applyTheme("accordionPadding")};
+`;
+
+const AccordionHeaderIcon = styled.span`
+  height: ${applyTheme("selectableListIconHeight")};
+  margin-bottom: 0;
+  margin-left: 0;
+  margin-right: ${applyTheme("selectableListIconSpacingToLabel")};
+  margin-top: 0;
+  width: ${applyTheme("selectableListIconWidth")};
+  svg {
+    height: ${applyTheme("selectableListIconHeight")};
+    transform: ${({ expanded }) => (expanded ? "rotateZ(180deg)" : "rotateZ(0)")};
+    transition: transform ${duration} ${ease};
+    width: ${applyTheme("selectableListIconWidth")};
+  }
+`;
+
+const AccordionContent = styled.div`
+  background-color: ${applyTheme("accordionContentBackgroundColor")};
+  color: inherit;
+  height: auto;
+  max-height: ${({ expanded }) => (expanded ? "200rem" : "0")};
+  overflow: hidden;
+  padding: ${({ expanded }) => (expanded ? applyTheme("accordionPadding") : "0")};
+  transition: ${({ expanded }) => (expanded ? openTransition : closeTransition)};
 `;
 
 class Accordion extends Component {
   static propTypes = {
+    children: PropTypes.any,
     /**
      * You can provide a `className` prop that will be applied to the outermost DOM element
      * rendered by this component. We do not recommend using this for styling purposes, but
@@ -22,16 +70,47 @@ class Accordion extends Component {
      * set up a components context or you want to override one of the components in a
      * single spot, you can pass in the components prop directly.
      */
-    components: PropTypes.shape({}).isRequired
+    components: PropTypes.shape({
+      iconExpand: CustomPropTypes.component.isRequired
+    }).isRequired,
+    detail: PropTypes.string,
+    expanded: PropTypes.bool,
+    icon: PropTypes.node,
+    label: PropTypes.string.isRequired
   };
 
-  static defaultProps = {};
+  static defaultProps = {
+    expanded: false
+  };
+
+  state = {
+    expanded: this.props.expanded
+  };
+
+  // handle accordion toggle
+  handleToggle = () => {
+    const { expanded } = this.state;
+    this.setState({ expanded: !expanded });
+  };
 
   render() {
-    const { className } = this.props;
+    const { children, className, components: { iconExpand }, detail, icon, label } = this.props;
+    const { expanded } = this.state;
 
-    return <StyledDiv className={className}>TEST</StyledDiv>;
+    return (
+      <AccordionWrapper className={className}>
+        <AccordionHeader onClick={this.handleToggle}>
+          <span>
+            {icon ? <AccordionHeaderIcon>{icon}</AccordionHeaderIcon> : null}
+            <b>{label}</b>
+            {detail ? `, ${detail}` : ""}
+          </span>
+          <AccordionHeaderIcon expanded={expanded}>{iconExpand}</AccordionHeaderIcon>
+        </AccordionHeader>
+        <AccordionContent expanded={expanded}>{children}</AccordionContent>
+      </AccordionWrapper>
+    );
   }
 }
 
-export default Accordion;
+export default withComponents(Accordion);
