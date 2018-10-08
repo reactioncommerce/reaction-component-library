2965d35ed198cb18a26307cb63ac755391860e7d
Eric Dobbertin <eric@dairystatedesigns.com>
2018-10-08 09:36:39 -0500

    docs: update GuestForm theme docs

--- package/src/components/GuestForm/v1/GuestForm.js
+++ package/src/components/GuestForm/v1/GuestForm.js
@@ -14,7 +14,7 @@ const FormAction = styled.div`
   > * {
     width: 100%;
 
-    @media (min-width: ${applyTheme("breakpoint_sm")}px) {
+    @media (min-width: ${applyTheme("sm", "breakpoints")}px) {
       width: auto;
     }
   }
