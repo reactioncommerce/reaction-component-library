39959a148e29f87dd86d765b50bbeb64ca3666a6
Nat Hamilton <info@nathamilton.com>
2018-10-08 10:41:27 -0500

    test: updated snapshots

--- package/src/components/AddressSelect/v1/AddressSelect.test.js
+++ package/src/components/AddressSelect/v1/AddressSelect.test.js
@@ -1,11 +1,12 @@
 import React from "react";
 import renderer from "react-test-renderer";
 // import { shallow } from "enzyme";
+import mockComponents from "../../../tests/mockComponents";
 import AddressSelect from "./AddressSelect";
 
 test("basic snapshot", () => {
-  const component = renderer.create(<AddressSelect />);
-
+  const mockAddressBook = [];
+  const component = renderer.create(<AddressSelect addressBook={mockAddressBook} components={mockComponents} />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
