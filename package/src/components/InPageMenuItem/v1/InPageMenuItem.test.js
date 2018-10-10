009cc2365d1c8be5dfa86ef04f8ed9c9d5aad280
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-10-10 14:20:05 -0700

    test: add tests for InPageMenutItem

--- package/src/components/InPageMenuItem/v1/InPageMenuItem.test.js
+++ package/src/components/InPageMenuItem/v1/InPageMenuItem.test.js
@@ -3,8 +3,23 @@ import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
 import InPageMenuItem from "./InPageMenuItem";
 
-test("basic snapshot", () => {
-  const component = renderer.create(<InPageMenuItem />);
+test("InPageMenuItem basic component", () => {
+  const component = renderer.create(<InPageMenuItem href="/test/url/" label="Test label" />);
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("InPageMenuItem with onClick instead of href", () => {
+  const onClick = () => {};
+  const component = renderer.create(<InPageMenuItem label="Test label" onClick={onClick} />);
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("InPageMenuItem selected", () => {
+  const component = renderer.create(<InPageMenuItem isSelected href="/test/url/" label="Test label" />);
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
