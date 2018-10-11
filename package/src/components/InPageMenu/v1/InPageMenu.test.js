9b27ca509502ca93d263f49664333fc58bce850a
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-10-10 20:15:38 -0700

    test: InPageMenu tests

--- package/src/components/InPageMenu/v1/InPageMenu.test.js
+++ package/src/components/InPageMenu/v1/InPageMenu.test.js
@@ -4,7 +4,31 @@ import { shallow } from "enzyme";
 import InPageMenu from "./InPageMenu";
 
 test("basic snapshot", () => {
-  const component = renderer.create(<InPageMenu />);
+  const menuItems = [
+    {
+      href: "/label/a",
+      label: "Label A"
+    },
+    {
+      href: "/label/b",
+      label: "Label B"
+    },
+    {
+      href: "/label/c",
+      label: "Label C (Selected / Active)",
+      isSelected: true
+    },
+    {
+      href: "/label/d",
+      label: "Label D"
+    },
+    {
+      href: "/label/e",
+      label: "Label E"
+    }
+  ];
+
+  const component = renderer.create(<InPageMenu menuItems={menuItems} />);
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
