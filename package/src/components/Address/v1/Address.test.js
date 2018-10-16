3850c90508110b6810f08e5e38eaeb9dc3f5277c
Nat Hamilton <info@nathamilton.com>
2018-10-16 15:39:56 -0500

    test: updated Address test and snapshot

--- package/src/components/Address/v1/Address.test.js
+++ package/src/components/Address/v1/Address.test.js
@@ -1,10 +1,22 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { shallow } from "enzyme";
+// import { shallow } from "enzyme";
 import Address from "./Address";
 
+const mockAddress = {
+  _id: "1",
+  address1: "7742 Hwy 23",
+  address2: "",
+  country: "US",
+  city: "Belle Chasse",
+  fullName: "Salvos Seafood",
+  postal: "70037",
+  region: "LA",
+  phone: "(504) 393-7303"
+};
+
 test("basic snapshot", () => {
-  const component = renderer.create(<Address />);
+  const component = renderer.create(<Address address={mockAddress} />);
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
