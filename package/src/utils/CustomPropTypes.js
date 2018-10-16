ecefdf6ab512e8afe83219b680920f13c614e80a
Nat Hamilton <info@nathamilton.com>
2018-10-16 08:34:20 -0500

    docs: updated address prop docs

--- package/src/utils/CustomPropTypes.js
+++ package/src/utils/CustomPropTypes.js
@@ -17,13 +17,11 @@ PropTypeError.prototype = Error.prototype;
 
 // These follow the composable form specification
 // http://forms.dairystatedesigns.com/user/input/#selection-inputs
-const optionsSyntax = PropTypes.arrayOf(
-  PropTypes.shape({
-    id: PropTypes.string,
-    label: PropTypes.string.isRequired,
-    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool]).isRequired
-  })
-);
+const optionsSyntax = PropTypes.arrayOf(PropTypes.shape({
+  id: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool]).isRequired
+}));
 
 // See https://github.com/facebook/prop-types/issues/200.
 // If that gets added to prop-types package, we won't need this.
@@ -33,19 +31,13 @@ function getComponentValidator() {
     if (val === null || val === undefined) {
       if (isRequired) {
         if (val === null) {
-          return new PropTypeError(
-            `The ${location} '${propFullName}' is marked as required in '${componentName}', but its value is 'null'.`
-          );
+          return new PropTypeError(`The ${location} '${propFullName}' is marked as required in '${componentName}', but its value is 'null'.`);
         }
-        return new PropTypeError(
-          `The ${location} '${propFullName}' is marked as required in '${componentName}', but its value is 'undefined'.`
-        );
+        return new PropTypeError(`The ${location} '${propFullName}' is marked as required in '${componentName}', but its value is 'undefined'.`);
       }
     } else if (!isValidElementType(val)) {
-      return new PropTypeError(
-        `Invalid ${location} '${propFullName}' supplied to ${componentName}. ` +
-          "Expected a string (for built-in components) or a class/function (for composite components)."
-      );
+      return new PropTypeError(`Invalid ${location} '${propFullName}' supplied to ${componentName}. ` +
+          "Expected a string (for built-in components) or a class/function (for composite components).");
     }
     return null;
   }
@@ -55,32 +47,66 @@ function getComponentValidator() {
   return chainedCheckType;
 }
 
-// address
+// address proptype
 const addressSyntax = PropTypes.shape({
+  /**
+   * Address ID (not all addreses will have an ID)
+   */
+  _id: PropTypes.string,
+  /**
+   * Name identifier for address (example: "Mom's House", "Work")
+   */
   addressName: PropTypes.string,
-  address1: PropTypes.string,
+  /**
+   * Street information for address
+   */
+  address1: PropTypes.string.isRequired,
+  /**
+   * Additional street information for address
+   */
   address2: PropTypes.string,
-  country: PropTypes.string,
-  city: PropTypes.string,
+  /**
+   * Country code for address
+   */
+  country: PropTypes.string.isRequired,
+  /**
+   * City information for address
+   */
+  city: PropTypes.string.isRequired,
+  /**
+   * User's name
+   */
   fullName: PropTypes.string,
+  /**
+   * Is this address a commercial property
+   */
   isCommercial: PropTypes.bool,
-  postal: PropTypes.string,
-  region: PropTypes.string,
+  /**
+   * Postal code information for address
+   */
+  postal: PropTypes.string.isRequired,
+  /**
+   * Region information for address
+   */
+  region: PropTypes.string.isRequired,
+  /**
+   * User's associated phone number
+   */
   phone: PropTypes.string
 });
 
-// address book
+// address book proptype
+const addressBookSyntax = PropTypes.arrayOf(addressSyntax);
 
 export default {
   address: addressSyntax,
+  addressBook: addressBookSyntax,
   component: getComponentValidator(),
   options: PropTypes.oneOfType([
     optionsSyntax,
-    PropTypes.arrayOf(
-      PropTypes.shape({
-        optgroup: PropTypes.string,
-        options: optionsSyntax
-      })
-    )
+    PropTypes.arrayOf(PropTypes.shape({
+      optgroup: PropTypes.string,
+      options: optionsSyntax
+    }))
   ])
 };
