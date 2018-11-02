a9806ddaf7793bb1dde16f448def766a89361664
Machiko Yasuda <machiko@reactioncommerce.com>
2018-11-02 09:20:11 -0700

    chore: lint

--- package/src/components/RegionInput/v1/RegionInput.test.js
+++ package/src/components/RegionInput/v1/RegionInput.test.js
@@ -11,9 +11,7 @@ test("basic snapshot with only the components and required props should render a
 });
 
 test("basic snapshot with other form props", () => {
-  const component = renderer.create(<RegionInput components={mockComponents} value="California" name="region" isOnDarkBackground
-    isReadOnly
-                                    />);
+  const component = renderer.create(<RegionInput components={mockComponents} value="California" name="region" isReadOnly />);
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
