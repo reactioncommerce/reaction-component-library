0754206af8dca68dc36fc5781b895a36c612fc03
Loan Laux <loan@outgrow.io>
2019-03-03 18:42:29 +0400

    feat: Add height theme prop to ShopLogo
    Signed-off-by: Loan Laux <loan@outgrow.io>

--- package/src/components/ShopLogo/v1/ShopLogo.js
+++ package/src/components/ShopLogo/v1/ShopLogo.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { addTypographyStyles } from "../../../utils";
+import { addTypographyStyles, applyTheme } from "../../../utils";
 
 const Container = styled.div`
   ${addTypographyStyles("ShopLogo", "titleTextBold")}
+`;
+
+const Logo = styled.img`
+  height: ${applyTheme("ShopLogo.height")};
 `;
 
 export default class ShopLogo extends Component {
@@ -26,7 +30,7 @@ export default class ShopLogo extends Component {
   }
 
   render() {
-    const { className, shopLogoUrl, shopName } = this.props;
+    const { className, height, shopLogoUrl, shopName } = this.props;
 
     return (
       <Container className={className}>
